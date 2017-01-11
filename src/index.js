56b8515e7fec7427812f442b36f64265d947fd73
Anatolii Ruchka <anatoliy.ruchka@gmail.com>
2017-01-11 13:47:12 +0300

    composeWithDevTools fail

--- src/index.js
+++ src/index.js
@@ -3,21 +3,27 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { Router, Route, browserHistory } from 'react-router';
-
+import { composeWithDevTools } from 'remote-redux-devtools';
 
 import App from './components/app';
 import Resources from './components/resources';
 import TodoComponent from './components/TodoComponent';
 import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+// const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = createStore(reducers, /* preloadedState, */ composeWithDevTools(
+  applyMiddleware(),
+  //applyMiddleware(...middleware),
+  //// other store enhancers if any
+));
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
+  //createStoreWithMiddleware(reducers)
+  <Provider store={createStoreWithMiddleware}>
     <Router history={browserHistory} >
       <Route path="/" component={App} />
-        <Route path="resources" component={Resources} />
-        <Route path="todo" component={TodoComponent} />
+      <Route path="resources" component={Resources} />
+      <Route path="todo" component={TodoComponent} />
     </Router>
   </Provider>
   , document.querySelector('.container-fluid'));
