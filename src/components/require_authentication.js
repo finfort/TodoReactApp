95fd2eb13f05dd68376934e02d396f5aee2cd52b
Anatolii Ruchka <anatoliy.ruchka@gmail.com>
2017-01-11 16:37:12 +0300

    add access to store authenticated by connect

--- src/components/require_authentication.js
+++ src/components/require_authentication.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-export default function(ComposedComponent){ //hire order component Authentication(ResourcesComponent)
+export default function(ComposedComponent){ //hire order component const Composedcomponent = Authentication(ResourcesComponent)  <ComposedComponent />
     class Authentication extends Component{
 
         render(){
+            console.log('rendering', this.props.authenticated);
             return <ComposedComponent {...this.props} />
         };
     }
 
-    return Authentication;
+    const mapStateToProps = (state, ownProps) => {
+        return {
+            authenticated: state.authenticated
+        }
+    }
+
+    return connect(mapStateToProps)(Authentication);
 }
+
--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { createStore, applyMiddleware } from 'redux';
 import { Router, Route, browserHistory } from 'react-router';
 import { composeWithDevTools } from 'remote-redux-devtools';
 
+import requireAuth from './components/require_authentication';
 import App from './components/app';
 import Resources from './components/resources';
 import TodoComponent from './components/TodoComponent';
@@ -22,7 +23,7 @@ ReactDOM.render(
   <Provider store={createStoreWithMiddleware}>
     <Router history={browserHistory} >
       <Route path="/" component={App} />
-      <Route path="resources" component={Resources} />
+      <Route path="resources" component={requireAuth(Resources)} />
       <Route path="todo" component={TodoComponent} />
     </Router>
   </Provider>
