fc479f4086aa6d688cca4e3a1cde24d1193bec9c
Anatolii Ruchka <anatoliy.ruchka@gmail.com>
2017-01-16 16:11:59 +0300

    making authentication

--- server/index.js
+++ server/index.js
@@ -2,9 +2,14 @@ const express = require('express');
 const http = require('http');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+
 
 const app = express();
 const router = require('./router');
+
+mongoose.connect('mongodb://localhost:auth/auth');
+
 
 app.use(morgan('combined')); // use login framework as middleware in express (logs incoming requests)
 app.use(bodyParser.json({type: '*/*'}));// parse http as json for logging
