d2efbfb890b97e74ac1709d6f7fc222ebe579969
Anatolii Ruchka <anatoliy.ruchka@gmail.com>
2017-01-18 11:08:05 +0300

    hook signin form

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,5 @@
 import { combineReducers } from 'redux';
+import { reducer as form } from 'redux-form'; // import for redux-form
 
 import authenticationReducer from './authentication';
 import todos from './todos';
@@ -9,7 +10,8 @@ const rootReducer = combineReducers({
   authenticated: authenticationReducer,
   todos,
   visibilityFilter,
-  users
+  users,
+  form
 });
 
 export default rootReducer;
