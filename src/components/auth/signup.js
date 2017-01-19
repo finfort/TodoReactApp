c81efd367a17acc0e39302cc8bb2209819e12bd9
Anatolii Ruchka <anatoliy.ruchka@gmail.com>
2017-01-19 17:27:25 +0300

    validate function

--- src/components/auth/signup.js
+++ src/components/auth/signup.js
@@ -1,17 +1,62 @@
 import React, { Component } from 'react';
-import { reduxForm } from 'redux-form';
+import { reduxForm, Field } from 'redux-form';
+import { connect } from 'react-redux';
+
 import * as actions from "../../actions";
 
 
 class Signup extends Component {
+
+    handleFormSubmit() {
+        // this.props.signinUser({ email, password });
+    }
+
+    renderAlert() {
+        if (this.props.errorMessage) {
+            return (
+                <div className="alert alert-danger">
+                    <strong>Oops!</strong> {this.props.errorMessage}
+                </div>
+            );
+        }
+    }
+
+
+
     render() {
+        const { handleSubmit } = this.props;
+
         return (
-            <div>
-                jj
-            </div>
+            <form onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
+                <fieldset className="form-group">
+                    <label htmlFor="email" className="control-label">Email:</label>
+                    <Field id="email" name="email" component="input" type="email" className="form-control" placeholder="Input email" />
+                </fieldset>
+                <fieldset className="form-group">
+                    <label htmlFor="password" className="control-label">Password:</label>
+                    <Field id="password" name="password" component="input" type="password" className="form-control" placeholder="Input password" />
+                </fieldset>
+                <fieldset className="form-group">
+                    <label htmlFor="passwordConfirm" className="control-label">Confirm password:</label>
+                    <Field id="passwordConfirm" name="passwordConfirm" component="input" type="password" className="form-control" placeholder="Input password" />
+                </fieldset>
+                {this.renderAlert()}
+                <button action="submit" className="btn btn-primary">Sign up</button>
+            </form>
         );
     }
 
 }
+
+function validate(formProps) {
+    const errors = {};
+
+    console.log(formProps);
+
+    return errors;
+}
+
+Signup = reduxForm({ form: 'signup', validate })(Signup);
+
 
 export default Signup
