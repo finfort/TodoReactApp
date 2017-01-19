264df71acdb58d670e4f7eb366b75ce1d7a86546
Anatolii Ruchka <anatoliy.ruchka@gmail.com>
2017-01-19 18:11:20 +0300

    problem with touched

--- src/components/auth/signup.js
+++ src/components/auth/signup.js
@@ -30,15 +30,18 @@ class Signup extends Component {
             <form onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
                 <fieldset className="form-group">
                     <label htmlFor="email" className="control-label">Email:</label>
-                    <Field id="email" name="email" component="input" type="email" className="form-control" placeholder="Input email" />
+                    <Field id="email" name="email" component="input" type="email" className="form-control" label="Input email" />
+                    {email.touched && ((error && <span>{error}</span>))}
                 </fieldset>
                 <fieldset className="form-group">
                     <label htmlFor="password" className="control-label">Password:</label>
-                    <Field id="password" name="password" component="input" type="password" className="form-control" placeholder="Input password" />
+                    <Field id="password" name="password" component="password" type="password" className="form-control" label="Input password" />
+                    {password.touched && ((error && <span>{error}</span>))}
                 </fieldset>
                 <fieldset className="form-group">
                     <label htmlFor="passwordConfirm" className="control-label">Confirm password:</label>
-                    <Field id="passwordConfirm" name="passwordConfirm" component="input" type="password" className="form-control" placeholder="Input password" />
+                    <Field id="passwordConfirm" name="passwordConfirm" component="password" type="password" className="form-control" label="Input password" />
+                    {passwordConfirm.touched && ((error && <span>{error}</span>))}
                 </fieldset>
                 {this.renderAlert()}
                 <button action="submit" className="btn btn-primary">Sign up</button>
@@ -48,10 +51,23 @@ class Signup extends Component {
 
 }
 
-function validate(formProps) {
+function validate(values) {
     const errors = {};
 
-    console.log(formProps);
+    if (!values.email) {
+        errors.email = 'Required'
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+        errors.email = 'Invalid email address'
+    }
+    if (!values.password)
+        errors.password = 'Required'
+
+    if (!values.passwordConfirm)
+        errors.passwordConfirm = 'Required'
+
+    if (values.password !== values.passwordConfirm) {
+        errors.password = "Passwords must match"
+    }
 
     return errors;
 }
